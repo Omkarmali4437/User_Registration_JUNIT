59b8ec0dbcb03b1358943e66642adfd7b5583031
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-03 11:34:05 +0530

    Added logic for validating last name in validate user last name

--- src/main/java/com/junit/UserRegistration.java
+++ src/main/java/com/junit/UserRegistration.java
@@ -4,9 +4,16 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public boolean result;
     public static String patternfirstname="[A-Z]{1}[a-z]{2,}";
+    public static String patternlastname="[A-Z]{1}[a-z]{2,}";
 
-    public boolean validateFirstName(String name) {
+    public boolean validateFirstName(String firstname) {
         Pattern pattern= Pattern.compile(patternfirstname);
-        return pattern.matcher(name).matches();
+        return pattern.matcher(firstname).matches();
     }
+    public boolean validateLastName(String lastname)
+    {
+        Pattern pattern= Pattern.compile(patternlastname);
+        return pattern.matcher(lastname).matches();
+    }
+
 }
--- src/test/java/com/junit/UserRegistrationTest.java
+++ src/test/java/com/junit/UserRegistrationTest.java
@@ -1,18 +1,35 @@
 package com.junit;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    UserRegistration user;
+    @Before
+    public void intialize() {
+        user=new UserRegistration();
+    }
+
     @Test
     public void given_first_name_check_true() {
-        UserRegistration user=new UserRegistration();
         boolean result=user.validateFirstName("Omkar");
         Assert.assertTrue(result);
     }
     @Test
     public void given_first_name_check_false() {
-        UserRegistration user=new UserRegistration();
         boolean result=user.validateFirstName("omkar");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void given_last_name_check_true() {
+        boolean result=user.validateLastName("Mali");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void given_last_name_check_false() {
+        boolean result=user.validateLastName("mali");
         Assert.assertFalse(result);
     }
 }
